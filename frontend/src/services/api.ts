c3a51beca75f61f4afbe62a1303e20a5a71a29a3
JoaoGui1430 <144964095+JoaoGui1430@users.noreply.github.com>
2026-05-03 12:36:36 -0300

    adicionando suporte para a url da api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { List, Task, TaskStatus } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3333/api',
 });
+
 
 // --- LISTS ---
 export const getLists = () =>
